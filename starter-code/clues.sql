56c8953583730f996d87c8f8029e76e9c1308756
jtreeves <jackson.t.reeves@gmail.com>
2020-11-13 15:28:54 -0500

    [sql] Answer first three clues with class during lab

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,20 +1,28 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
-
-
+-- ANSWER:
+-- Vatican City (VAT)
+-- QUERY:
+SELECT * FROM countries WHERE region = 'Southern Europe' ORDER BY population ASC LIMIT 1;
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
+-- ANSWER:
+-- Italian
+-- QUERY:
+SELECT * FROM countrylanguages WHERE countrycode = 'VAT';
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 -- Hint: first find the countries where only one language is spoken. Then, in a separate query, find the countries where Italian is spoken. Then cross reference these lists by hand.
-
+-- ANSWER:
+-- San Marino
+-- QUERY:
+SELECT countrycode FROM countrylanguages WHERE language = 'Italian' AND percentage = 100;
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
